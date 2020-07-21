22b17c230566ccc9975ce9756b8825c94874832f
EricksonLopes <ofc.erickson@gmail.com>
2020-07-21 09:15:03 -0300

    lecture 10 - Rendering the first template

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog.view import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+    path('', include("website.urls")),
 ]
