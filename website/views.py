55b95a070d8ba5c3bd99daab545816db5b0b10aa
EricksonLopes <ofc.erickson@gmail.com>
2020-07-21 09:36:10 -0300

    lecture 11 - Understanding Django's template system better

--- website/views.py
+++ website/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def hello_blog(request):
-    return render(request, 'index.html')
+    lista = ['django', 'python', 'git', 'HTML', 'linux', 'Nginux', 'Banco de dados',  'Systemctl']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
