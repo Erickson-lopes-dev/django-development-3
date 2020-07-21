535c70be899d4db17247517d34b3c802e03a9803
EricksonLopes <ofc.erickson@gmail.com>
2020-07-21 09:54:43 -0300

    lecture 12 - Reading posts from the database

--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def hello_blog(request):
     lista = ['django', 'python', 'git', 'HTML', 'linux', 'Nginux', 'Banco de dados',  'Systemctl']
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.all()
+
+    data = {
+        'name': 'Curso de Django 3',
+        'lista_tecnologias': lista,
+        'posts': list_posts
+    }
+
     return render(request, 'index.html', data)
