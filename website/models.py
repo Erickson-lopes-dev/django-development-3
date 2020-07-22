1be6832049469075ab0651768f387ac5f0eb31ec
EricksonLopes <ofc.erickson@gmail.com>
2020-07-22 15:29:44 -0300

    27/5000 lesson 21 - Media files

--- website/models.py
+++ website/models.py
@@ -18,6 +18,8 @@ class Post(models.Model):
     )
     approved = models.BooleanField(default=True)
 
+    imagem = models.ImageField(upload_to='posts', null=True, blank=True)
+
     def __str__(self):
         return self.title
 
