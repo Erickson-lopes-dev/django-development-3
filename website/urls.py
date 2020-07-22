e60a8d0b80f45a8022813790fd42a45570094877
EricksonLopes <ofc.erickson@gmail.com>
2020-07-22 14:43:57 -0300

    lecture 20 - Creating the post details page

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', hello_blog)
+    path('', hello_blog, name='posts'),
+    path('post/<int:id>', post_detail, name='post_detail')
 ]
--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 
 def hello_blog(request):
-    lista = ['django', 'python', 'git', 'HTML', 'linux', 'Nginux', 'Banco de dados',  'Systemctl']
+    lista = ['django', 'python', 'git', 'HTML', 'linux', 'Nginux', 'Banco de dados', 'Systemctl']
     list_posts = Post.objects.filter(approved=True)
     # list_posts = Post.objects.all()
 
@@ -14,3 +14,8 @@ def hello_blog(request):
     }
 
     return render(request, 'index.html', data)
+
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post_detail.html', {'post': post})
